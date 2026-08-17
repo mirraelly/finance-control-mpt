05526a161e3a2e0957fbdbeb09b5e1eb08f1cb7f
julianaviguis <julianaviguis.contato@gmail.com>
2026-08-17 20:25:43 -0300

    wip: estrutura da tela de cadastro, integracao e ajuste de rotas

--- financecontrol-front/src/routes/AppRoutes.jsx
+++ financecontrol-front/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
+import Cadastro from '../pages/Cadastro/Cadastro'
 
 function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/Cadastro" element={<Cadastro />} />
       </Routes>
     </BrowserRouter>
   );
